0d0bd85c2659ad6090908ac8e7510cc54cf6bef2
Dmitry Shevtsov <dshevtsov20@gmail.com>
2020-10-03 14:42:21 +0300

    feat: min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 
-exports.min = function min (array) {
+exports.min = function min (array) { 
+  var min
+  try {
+    if (array.length != 0) {
+      min =  Math.min.apply(Math,array);
+    }
+    else{
+      throw Error
+    }
+    
+  } catch (error) {
+    min = 0
+  }
   
-  return 0;
+  
+  return min
 }
 
 exports.max = function max (array) {
-  return 0;
+  var max
+  try {
+    if (array.length != 0) {
+      max =  Math.max.apply(Math,array);
+    }
+    else{
+      throw Error
+    }
+  } catch (error) {
+    max = 0
+  }
+  return max
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  try {
+    var avg
+    if (array.length != 0) {
+      avg = array.reduce((a,b) => a + b, 0)/array.length
+    }
+    else{
+      throw Error
+    }
+    
+  } catch (error) {
+    avg = 0
+  }
+  return avg
 }
